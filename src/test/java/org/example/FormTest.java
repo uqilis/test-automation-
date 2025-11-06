84ff724fa0727e5489393dcce442fcb9f5968280
uqilis <erinaiulia5@gmail.com>
2025-11-06 15:35:56 +0200

    Fix gender click

--- src/test/java/org/example/FormTest.java
+++ src/test/java/org/example/FormTest.java
@@ -3,6 +3,7 @@ package org.example;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
@@ -27,12 +28,23 @@ public class FormTest {
     public void fillFormTest() {
         driver.get(URL);
 
-        FormPom form = new FormPom(driver );
+         try {
+            WebElement ad = driver.findElement(By.cssSelector("iframe[id^='google_ads_iframe']"));
+            ((JavascriptExecutor) driver).executeScript("arguments[0].style.display='none';", ad);
+        } catch (Exception e) {
+
+        }
+
+        FormPom form = new FormPom(driver);
 
         form.setFirstName("Iulia");
         form.setLastName("Erina");
         form.setEmail("erina.iulia5@gmail.com");
-        form.setFemaleGender();
+
+        WebElement female = driver.findElement(By.cssSelector("label[for='gender-radio-2']"));
+        ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", female);
+        ((JavascriptExecutor) driver).executeScript("arguments[0].click();", female);
+
         form.setMobile("0676785670");
         form.setDateOfBirth("07 Nov 2006");
         form.setSubjects("Maths");
@@ -42,14 +54,13 @@ public class FormTest {
         form.setCity("Panipat");
         form.clickSubmit();
 
-        // Scroll top pentru a evita ca modalul să fie în afara vizualizării
+
         ((JavascriptExecutor) driver).executeScript("window.scrollTo(0, 0)");
 
-        // Așteaptă apariția modalului (titlul modalului)
+
         new WebDriverWait(driver, Duration.ofSeconds(8))
                 .until(ExpectedConditions.visibilityOfElementLocated(By.id("example-modal-sizes-title-lg")));
 
-        // Verificăm numele introdus
         String actualName = driver.findElement(By.xpath("//td[text()='Student Name']/following-sibling::td")).getText();
         Assert.assertEquals(actualName, "Iulia Erina", "Numele introdus nu corespunde!");
     }
--- src/test/java/utils/Driver.java
+++ src/test/java/utils/Driver.java
@@ -11,7 +11,7 @@ public class Driver {
         if (driver == null) {
             ChromeOptions options = new ChromeOptions();
 
-           
+
             options.addArguments("--headless=new");
             options.addArguments("--no-sandbox");
             options.addArguments("--disable-dev-shm-usage");
