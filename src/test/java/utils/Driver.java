a641b033db0a7aa3ed49b611f8279385922b5940
uqilis <erinaiulia5@gmail.com>
2025-11-06 15:22:41 +0200

    Run Chrome in headless mode for GitHub Actions

--- src/test/java/utils/Driver.java
+++ src/test/java/utils/Driver.java
@@ -2,11 +2,24 @@ package utils;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class Driver {
+    private static WebDriver driver;
+
     public static WebDriver getLocalDriver() {
-        WebDriverManager.chromedriver().setup();
-        return new ChromeDriver();
+        if (driver == null) {
+            ChromeOptions options = new ChromeOptions();
+
+           
+            options.addArguments("--headless=new");
+            options.addArguments("--no-sandbox");
+            options.addArguments("--disable-dev-shm-usage");
+            options.addArguments("--disable-gpu");
+            options.addArguments("--window-size=1920,1080");
+
+            driver = new ChromeDriver(options);
+        }
+        return driver;
     }
 }
